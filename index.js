281296f8c7cce067cd35e64667cb13b7002eb88f
Sunny4322 <ks3411262@gmail.com>
2022-11-25 12:51:05 +0530

    post-method

---
+++ index.js
@@ -0,0 +1,21 @@
+const express=require("express")
+const app=express()
+const cors=require("cors")
+const bodyParser=require("body-parser")
+const port=3000
+
+app.use(bodyParser.urlencoded({
+	extended:true
+}))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get("/",(req,res)=>{
+	res.send("pavankalyan")
+})
+app.post("/newData",(req,res)=>{
+	const{name,password}=req.body;
+	console.log(name,password)
+	res.send("added")
+})
+app.listen(port,()=>console.log("server is running on port 3000"))
